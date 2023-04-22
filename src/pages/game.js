20f50527672f2d6ff4a6458dcd077837fbdee47d
Jae Lee <jaehlee.110994@gmail.com>
2023-04-22 18:05:44 -0400

    done for today

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,118 +1,25 @@
+import { useMemo } from "react"
+import { createAvatar } from "@dicebear/core"
+import { lorelei } from "@dicebear/collection"
 import Image from "next/image"
 
 export default function Game() {
-  var textStyle = {
-    color: "white",
-    textAlign: "left",
-    fontSize: "24px",
-  }
-
-  var containerStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  }
-
-  var gameContainerStyle = {
-    backgroundColor: "rgba(256, 256, 256, 0.7)",
-    width: "45%",
-    height: "70vh",
-    marginLeft: "200px",
-    marginRight: "100px",
-    marginBottom: "20px",
-    marginTop: "40px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
-  }
-
-  var boxContainerStyle = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    padding: "10px",
-  }
-
-  var roleStyle = {
-    backgroundColor: "rgba(0, 0, 0, 0.1)",
-    height: "100px",
-    margin: "0 5px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flex: "0.2",
-  }
-
-  var timerStyle = {
-    backgroundColor: "transparent",
-    height: "100px",
-    margin: "0 5px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flex: "1",
-  }
-
-  var dayStyle = {
-    backgroundColor: "rgba(0, 0, 0, 0.1)",
-    height: "100px",
-    margin: "0 5px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flex: "0.2",
-  }
-
-  var chatContainerStyle = {
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
-    width: "35%",
-    height: "70vh",
-    marginLeft: "auto",
-    marginRight: "300px",
-    marginBottom: "20px",
-    marginTop: "40px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-start",
-    padding: "16px",
-    overflowY: "auto",
-    textAlign: "left",
-  }
-
-  var chatContentContainerStyle = {
-    flex: "1",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-end",
-  }
-
-  var inputContainerStyle = {
-    display: "flex",
-    marginTop: "16px",
-    justifyContent: "flex-end",
-    marginRight: "300px",
-  }
-
-  var inputStyle = {
-    backgroundColor: "grey",
-    color: "white",
-    border: "none",
-    flex: "1",
-    padding: "8px",
-    maxWidth: "30%",
-  }
-
-  var buttonStyle = {
-    backgroundColor: "grey",
-    color: "white",
-    border: "none",
-    marginLeft: "8px",
-    padding: "8px",
-  }
-
   var insideChat = [{ username: "User1", text: "HAHA EASY GAME" }]
+
+  const avatar = useMemo(() => {
+    return createAvatar(lorelei, {
+      size: 128,
+      seed: "John",
+    }).toDataUriSync()
+  }, [])
+
+  const avatar1 = useMemo(() => {
+    return createAvatar(lorelei, {
+      size: 128,
+      seed: "Amy",
+    }).toDataUriSync()
+  }, [])
+
   return (
     <>
       <div style={containerStyle}>
@@ -128,6 +35,41 @@ export default function Game() {
               <p>Day 🔆/Night 🌙</p>
             </div>
           </div>
+          <div className="players" style={playerContainer}>
+            <div style={player}>
+              <Image src={avatar} alt="Avatar" width="100" height="100" />
+              <small>user1</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar1} alt="Avatar" width="100" height="100" />
+              <small>user2</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar} alt="Avatar" width="100" height="100" />
+              <small>user1</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar1} alt="Avatar" width="100" height="100" />
+              <small>user2</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar} alt="Avatar" width="100" height="100" />
+              <small>user1</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar1} alt="Avatar" width="100" height="100" />
+              <small>user2</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar} alt="Avatar" width="100" height="100" />
+              <small>user1</small>
+            </div>
+            <div style={player}>
+              <Image src={avatar1} alt="Avatar" width="100" height="100" />
+              <small>user2</small>
+            </div>
+          </div>
+          <div style={werewolfTextContainer}>Werewolves: user1, user2</div>
         </div>
         <div style={chatContainerStyle}>
           <div style={chatContentContainerStyle}>
@@ -151,4 +93,134 @@ export default function Game() {
       </div>
     </>
   )
+}
+
+var textStyle = {
+  color: "white",
+  textAlign: "left",
+  fontSize: "24px",
+}
+
+var containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}
+
+var gameContainerStyle = {
+  backgroundColor: "rgba(256, 256, 256, 0.7)", // CHANGE COLOR HERE THROUGH STATE
+  width: "45%",
+  height: "70vh",
+  marginLeft: "200px",
+  marginRight: "100px",
+  marginBottom: "20px",
+  marginTop: "40px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  position: "relative",
+}
+
+var boxContainerStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  padding: "10px",
+}
+
+var roleStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.1)",
+  height: "100px",
+  margin: "0 5px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flex: "0.2",
+}
+
+var timerStyle = {
+  backgroundColor: "transparent",
+  height: "100px",
+  margin: "0 5px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flex: "1",
+}
+
+var dayStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.1)",
+  height: "100px",
+  margin: "0 5px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flex: "0.2",
+}
+
+var playerContainer = {
+  display: "grid",
+  marginTop: "30px",
+  gridTemplateColumns: "repeat(4, auto)",
+  gap: "20px",
+}
+
+var player = {
+  textAlign: "center",
+}
+
+var werewolfTextContainer = {
+  position: "absolute",
+  bottom: "10px",
+  right: "10px",
+  textAlign: "right",
+}
+
+var chatContainerStyle = {
+  backgroundColor: "rgba(0, 0, 0, 0.7)",
+  width: "35%",
+  height: "70vh",
+  marginLeft: "auto",
+  marginRight: "300px",
+  marginBottom: "20px",
+  marginTop: "40px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  padding: "16px",
+  overflowY: "auto",
+  textAlign: "left",
+}
+
+var chatContentContainerStyle = {
+  flex: "1",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-end",
+}
+
+var inputContainerStyle = {
+  display: "flex",
+  marginTop: "16px",
+  justifyContent: "flex-end",
+  marginRight: "300px",
+}
+
+var inputStyle = {
+  backgroundColor: "grey",
+  color: "white",
+  border: "none",
+  flex: "1",
+  padding: "8px",
+  maxWidth: "30%",
+}
+
+var buttonStyle = {
+  backgroundColor: "grey",
+  color: "white",
+  border: "none",
+  marginLeft: "8px",
+  padding: "8px",
 }
