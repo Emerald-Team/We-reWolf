56655ba3579811bbdf4df1dea77f01f6d2bdff05
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-04-24 19:19:45 -0400

    fixed redux isseus

--- src/redux/gameSlice.js
+++ src/redux/gameSlice.js
@@ -1,7 +1,7 @@
+import {combineReducers} from 'redux'
 import {createSlice} from '@reduxjs/toolkit'
 
 const initialState = {
-  {
     "users": {
      "username": {
         "user": {},
@@ -18,5 +18,55 @@ const initialState = {
     "doctors": [],
     "seers": [],
     "lobbyId": 1
-  }
 }
+
+export const gameSlice = createSlice({
+  name: 'gameState',
+  initialState: initialState,
+  reducers: {
+    // increment: state => {
+    //   state.value += 1
+    // },
+    // decrement: state => {
+    //   state.value -= 1
+    // },
+    // addUser: (state, action) => {
+    //   state.users[action.payload.username] = action.payload
+    // },
+
+    // setRole: (state, action) => {
+    //   state.users[action.payload.username].role = action.payload.role
+    // },
+
+    // killUser: (state, action) => {
+    //   const user = state.users[action.payload]
+    //   user.alive = false,
+    //   user.markedForDeath = false
+    // },
+
+    // markForDeath: (state, action) => {
+    //   state.users[action.payload.username].markedForDeath = action.payload.bool
+    //  },
+
+    setTimeOfDay: (state, action) => {
+      console.log('in setTimeOfDay')
+      state.timeOFDay = action.payload
+    },
+
+    // voteToKill:(state, action) => {
+    //   state.users[action.payload.username].votesToKill++
+    // },
+
+    // removeVoteToKill: (state, action) => {
+    //   state.users[action.payload.username].votesToKill--
+    // },
+
+    // resetVotes: (state) => {
+    //   Object.values(state.users).forEach(user => {
+    //     user.votesToKill = 0;
+    //   })
+    // }
+  }
+})
+
+export const {setTimeOfDay} = gameSlice.actions
--- src/redux/stores.js
+++ src/redux/stores.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import gameReducer from './gameSlice'
 
-export const store = configureStore({
-  reducer: {},
+const rootReducer = combineReducers({
+  gameState: gameReducer,
+  // add other reducers here if you have any
 })
+
+const store = configureStore({
+  reducer: rootReducer
+})
+
+export default store
