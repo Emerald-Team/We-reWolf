efd9e6d03f749ee42a99eda99963bd2c0e3fb468
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-04-24 14:37:39 -0400

    additional cahnges to redux file

--- src/redux/gameSlice.js
+++ src/redux/gameSlice.js
@@ -20,3 +20,34 @@ const initialState = {
     "lobbyId": 1
   }
 }
+
+export const gameSlice = createSlice({
+  name: 'gameState',
+  initialState: initialState,
+  reducers: {
+    // increment: state => {
+    //   state.value += 1
+    // },
+    // decrement: state => {
+    //   state.value -= 1
+    // },
+    addUser: (state, action) => {
+      state.users[action.payload.username] = action.payload
+    },
+    setRole: (state, action) => {
+      state.users[action.payload.username].role = action.payload.role
+    },
+    killUser: (state, action) => {
+      const user = state.users[action.payload]
+      user.alive = false,
+      user.markedForDeath = false
+    },
+    markForDeath(state, action) => {
+      state.users[action.payload.username].markedForDeath = action.payload.bool
+     },
+
+    setTimeOfDay: (state, action) => {
+      state.timeOfDay = action.payload
+    }
+  }
+})
