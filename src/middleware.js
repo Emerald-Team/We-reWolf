84139a7ecb6e90488951d56d66b43c3c8a421d50
Ben Tracy <b.rob.tracy@gmail.com>
2023-04-25 14:57:46 -0400

    make dynamic game route

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,17 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 
-export function middleware(request) {
-  return NextResponse
+export function middleware(req) {
+  const res = NextResponse.next()
+  if(req.url === '/api/join/:gameID*'){
+    const { gameID } = "1234"
+    res.cookies.set("wwGameID", gameID)
+    return res
+  }
+
+
 }
 
 export const config = {
-  matcher: '/about/:path*',
+  matcher: ['/api/join/:gameID*', '/game', '/lobby']
 }
