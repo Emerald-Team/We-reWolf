a34b0373467f8e6823f2119b687c39e6e9035b93
Ben Tracy <b.rob.tracy@gmail.com>
2023-04-25 16:37:31 -0400

    merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import App from "./_app"
-
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const router = useRouter()
+
   const styles = {
     container: {
       display: "flex",
@@ -58,14 +65,32 @@ export default function Home() {
     },
   }
 
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    if(username === 'admin' && password === 'password'){
+      console.log(username, password)
+      router.push('/lobby')
+    } else {
+      console.log('sorrie, charlie')
+    }
+  }
+
   return (
     <div style={styles.container}>
       <div style={styles.loginBox}>
         <h2 style={{ marginBottom: "10px" }}>Login</h2>
         <form style={styles.form}>
-          <input type="text" placeholder="Username" style={styles.input} />
-          <input type="password" placeholder="Password" style={styles.input} />
-          <button type="submit" style={styles.button}>
+          <input type="text" placeholder="Username" onChange={handleUsername} style={styles.input} />
+          <input type="password" placeholder="Password" onChange={handlePassword} style={styles.input} />
+          <button type="submit" style={styles.button} onClick={handleLogin}>
             Login
           </button>
         </form>
