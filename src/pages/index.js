c6a1fb006cc9cb5138e4dbcf80c6a8ab558800d5
Ben Tracy <b.rob.tracy@gmail.com>
2023-04-26 14:49:29 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google"
 import App from "./_app"
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import axios from 'axios'
 
 
 const inter = Inter({ subsets: ["latin"] })
