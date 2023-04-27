731cde6fc88605152d4600a141b751818eeeae11
Justen Lane <justen.lane@gmail.com>
2023-04-27 11:05:04 -0400

    Ready Up Button Get Post Put routes

--- src/pages/api/lobby/[gameID].js
+++ src/pages/api/lobby/[gameID].js
@@ -3,6 +3,7 @@ import model from '../../../server/model.js'
 const handler = async (req, res) => {
   const { gameID } = req.query
   if(req.method === 'PUT'){
+    console.log(`PUT request received in API for ${gameID} for ${req.body.user}`)
     model.updateLobby(gameID, req.body.user)
     .then(data => (
       res.status(201).send(data)
@@ -11,6 +12,8 @@ const handler = async (req, res) => {
       res.status(401).send(err)
     ))
   } else if (req.method === 'POST'){
+    console.log(`POST request received in API for ${gameID} for ${req.body.user}`)
+
     model.createLobby(gameID, req.body.user)
     .then(data => (
       res.status(201).send(data)
