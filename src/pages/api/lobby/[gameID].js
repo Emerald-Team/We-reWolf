39679b167261402e5cd31436efd225e75264d8bf
Ben Tracy <b.rob.tracy@gmail.com>
2023-04-27 17:14:13 -0400

    Update [gameID].js

--- src/pages/api/lobby/[gameID].js
+++ src/pages/api/lobby/[gameID].js
@@ -1,6 +1,6 @@
 import model from '../../../server/model.js'
 
-const handler = async (req, res) => {
+export default async function handler(req, res) {
   const { gameID } = req.query
   if(req.method === 'PUT'){
 
